44a4dfb6b5535c910a5e66a52061c662bf391a69
John Chilton <jmchilton@gmail.com>
2013-02-26 23:03:43 -0600

    Add property to get state map from histories, figure out which HDAs are in what state - ok, failed, etc....

--- src/main/java/com/github/jmchilton/blend4j/galaxy/beans/HistoryDetails.java
+++ src/main/java/com/github/jmchilton/blend4j/galaxy/beans/HistoryDetails.java
@@ -1,10 +1,15 @@
 package com.github.jmchilton.blend4j.galaxy.beans;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+import org.codehaus.jackson.annotate.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown=true)
 public class HistoryDetails extends History {
   private String state;
+  private Map<String, List<String>> stateIds = new HashMap<String, List<String>>();
 
   public String getState() {
     return state;
@@ -13,5 +18,15 @@ public class HistoryDetails extends History {
   public void setState(String state) {
     this.state = state;
   }
+  
+  @JsonProperty("state_ids")
+  public void setStateIds(final Map<String, List<String>> stateIds) {
+    this.stateIds = stateIds;
+  }
+  
+  public Map<String, List<String>> getStateIds() {
+    return stateIds;
+  }
+
   
 }
