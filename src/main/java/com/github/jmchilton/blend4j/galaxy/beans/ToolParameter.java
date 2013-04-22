ec3cb028e8d6e4df2567c110231b67396f7c331c
John Chilton <jmchilton@gmail.com>
2013-04-22 14:17:10 -0500

    Revert styling changes introduced with 9d641ea9cb1c0337af1359bc9f355dfe7959736a.

--- src/main/java/com/github/jmchilton/blend4j/galaxy/beans/ToolParameter.java
+++ src/main/java/com/github/jmchilton/blend4j/galaxy/beans/ToolParameter.java
@@ -1,48 +1,37 @@
 package com.github.jmchilton.blend4j.galaxy.beans;
 
-
 import org.codehaus.jackson.annotate.JsonProperty;
 
-
-/**
- * Created with IntelliJ IDEA.
- * User: mlogghe
- * Date: 4/3/13
- * Time: 6:06 PM
- * To change this template use File | Settings | File Templates.
+/**                                                                                                                                                                          
+ *                                                                                                                                                                           
+ * @author Mark Logghe
+ *                                                                                                                                                                           
  */
-public class ToolParameter
-{
+public class ToolParameter {
+
   private String parameterName;
   private String parameterValue;
 
-  public ToolParameter(String parameterName, String parameterValue)
-  {
+  public ToolParameter(String parameterName, String parameterValue) {
     this.parameterName = parameterName;
     this.parameterValue = parameterValue;
   }
 
   @JsonProperty("param")
-  public String getParameterName()
-  {
+  public String getParameterName() {
     return parameterName;
   }
 
-  public void setParameterName(String parameterName)
-  {
+  public void setParameterName(String parameterName) {
     this.parameterName = parameterName;
   }
 
   @JsonProperty("value")
-  public String getParameterValue()
-  {
+  public String getParameterValue() {
     return parameterValue;
   }
 
-  public void setParameterValue(String parameterValue)
-  {
+  public void setParameterValue(String parameterValue) {
     this.parameterValue = parameterValue;
   }
 }
-
-
