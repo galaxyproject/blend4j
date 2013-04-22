0c5b3573c400eb5b8fcc2a9047e1b3e1f36ccd7d
John Chilton <jmchilton@gmail.com>
2013-04-22 14:09:40 -0500

    Add outline of HistoriesTest, this needs to be populated.

---
+++ src/test/java/com/github/jmchilton/blend4j/galaxy/HistoriesTest.java
@@ -0,0 +1,13 @@
+package com.github.jmchilton.blend4j.galaxy;
+
+import org.testng.annotations.Test;
+
+public class HistoriesTest {
+
+  @Test
+  public void testShowHistory() {
+    //final HistoriesClient client = IntegrationTest.getHistoriesClient();
+    // TODO:
+  }
+  
+}
