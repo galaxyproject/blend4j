ee8585efc45c8ed87501ba6e9c1335e8cfb89bdd
John Chilton <jmchilton@gmail.com>
2013-08-29 22:01:30 -0500

    Refactor most non-Galaxy specific functionality out of ClientImpl into a new BaseClient class in com.github.jmchilton.blend4j to prepare for a Tool Shed API.

---
+++ src/main/java/com/github/jmchilton/blend4j/BaseClient.java
@@ -0,0 +1,115 @@
+package com.github.jmchilton.blend4j;
+
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.multipart.BodyPart;
+import com.sun.jersey.multipart.FormDataBodyPart;
+import com.sun.jersey.multipart.FormDataMultiPart;
+import com.sun.jersey.multipart.file.FileDataBodyPart;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import javax.ws.rs.core.MediaType;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.type.TypeReference;
+
+public class BaseClient {
+  protected final ObjectMapper mapper = new ObjectMapper();
+  protected final WebResource webResource;
+
+  public BaseClient(final WebResource baseWebResource,
+                    final String module) {
+    this.webResource = baseWebResource.path(module);
+  }
+
+  protected ClientResponse create(final Object object) {
+    return create(getWebResource(), object);
+  }
+
+  protected ClientResponse create(final WebResource webResource, final Object object) {
+    return webResource.type(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON).post(ClientResponse.class, object);
+  }
+
+  protected <T> List<T> get(final WebResource webResource, final TypeReference<List<T>> typeReference) {
+    final String json = getJson(webResource);
+    return readJson(json, typeReference);
+  }
+
+  protected <T> List<T> get(final TypeReference<List<T>> typeReference) {
+    return get(getWebResource(), typeReference);
+  }
+
+  protected String getJson(final WebResource webResource) {
+    final String json = webResource.accept(MediaType.APPLICATION_JSON).get(String.class);
+    return json;
+  }
+
+  protected WebResource getWebResource() {
+    return webResource;
+  }
+
+  protected WebResource getWebResource(final String id) {
+    return getWebResource().path(id);
+  }
+
+  protected WebResource getWebResourceContents(final String id) {
+    return getWebResource(id).path("contents");
+  }
+
+  protected <T> TypeReference<List<T>> listTypeReference(final Class<T> clazz) {
+    return new TypeReference<List<T>>() {
+    };
+  }
+
+  protected ClientResponse multipartPost(final WebResource resource, final Map<String, Object> fields, final Iterable<BodyPart> bodyParts) {
+    final WebResource.Builder builder = resource.type(MediaType.MULTIPART_FORM_DATA_TYPE);
+    final FormDataMultiPart multiPart = new FormDataMultiPart();
+    for(final Map.Entry<String, Object> fieldEntry : fields.entrySet()) {
+      final FormDataBodyPart bp = new FormDataBodyPart(fieldEntry.getKey(), fieldEntry.getValue(), MediaType.APPLICATION_JSON_TYPE);
+      multiPart.bodyPart(bp);
+    }
+    for(final BodyPart bodyPart : bodyParts) {
+      bodyPart.setMediaType(MediaType.APPLICATION_OCTET_STREAM_TYPE);
+      multiPart.bodyPart(bodyPart);
+    }
+    return builder.post(ClientResponse.class, multiPart);
+  }
+
+  protected Iterable<BodyPart> prepareUpload(final File file) {
+    return prepareUploads(Arrays.asList(file));
+  }
+
+  protected Iterable<BodyPart> prepareUploads(final Iterable<File> files) {
+    final List<BodyPart> bodyParts = new ArrayList<BodyPart>();
+    for(final File file : files) {
+      final String paramName = "files_0|file_data";
+      final FileDataBodyPart fdbp = new FileDataBodyPart(paramName, file);
+      bodyParts.add(fdbp);
+    }
+    return bodyParts;
+  }
+
+  protected <T> T readJson(final String json, final TypeReference<T> typeReference) {
+    try {
+      return mapper.readValue(json, typeReference);
+    } catch(IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  protected <T> T show(final String id, Class<T> clazz) {
+    return getWebResource().path(id).get(clazz);
+  }
+
+  protected String write(final Object object) {
+    try {
+      return mapper.writer().writeValueAsString(object);
+    } catch(IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+}
--- src/main/java/com/github/jmchilton/blend4j/galaxy/ClientImpl.java
+++ src/main/java/com/github/jmchilton/blend4j/galaxy/ClientImpl.java
@@ -1,146 +1,24 @@
 package com.github.jmchilton.blend4j.galaxy;
 
+import com.github.jmchilton.blend4j.BaseClient;
 import com.github.jmchilton.blend4j.galaxy.beans.HasGalaxyUrl;
-import com.sun.jersey.api.client.ClientRequest;
-import java.io.IOException;
-import java.util.List;
 
-import javax.ws.rs.core.MediaType;
-
-import org.codehaus.jackson.map.ObjectMapper;
-import org.codehaus.jackson.type.TypeReference;
-
-import com.sun.jersey.api.client.ClientResponse;
-import com.sun.jersey.api.client.WebResource;
-import com.sun.jersey.multipart.BodyPart;
-import com.sun.jersey.multipart.FormDataBodyPart;
-import com.sun.jersey.multipart.FormDataMultiPart;
-import com.sun.jersey.multipart.file.FileDataBodyPart;
-import java.io.File;
-import java.net.URLEncoder;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Map;
-
-class ClientImpl {
+class ClientImpl extends BaseClient {
   private final GalaxyInstanceImpl galaxyInstance;
-  private final WebResource webResource;
-  private final ObjectMapper mapper = new ObjectMapper();
 
   ClientImpl(final GalaxyInstanceImpl galaxyInstance, final String module) {
+    super(galaxyInstance.getWebResource(), module);
     this.galaxyInstance = galaxyInstance;
-    this.webResource = buildWebResource(module);
-  }
-
-  protected String write(final Object object) {
-    try {
-      return mapper.writer().writeValueAsString(object);
-    } catch(IOException e) {
-      throw new RuntimeException(e);
-    }
   }
 
   GalaxyInstance getGalaxyInstance() {
     return galaxyInstance;
   }
-
-  protected WebResource getWebResource() {
-    return webResource;
-  }
-
-  protected WebResource getWebResource(final String id) {
-    return getWebResource().path(id);
-  }
-
-  protected WebResource getWebResourceContents(final String id) {
-    return getWebResource(id).path("contents");
-  }
-
-  private WebResource buildWebResource(final String module) {
-    final WebResource webResource = galaxyInstance.getWebResource();
-    return webResource.path(module);
-  }
-
-  protected <T> T readJson(final String json, final TypeReference<T> typeReference) {
-    try {
-      return mapper.readValue(json, typeReference);
-    } catch(IOException e) {
-      throw new RuntimeException(e);
-    }
-  }
-
-  protected ClientResponse create(final Object object) {
-    return create(getWebResource(), object);
-  }
-
-  protected ClientResponse create(final WebResource webResource, final Object object) {
-    return webResource
-            .type(MediaType.APPLICATION_JSON)
-            .accept(MediaType.APPLICATION_JSON)
-            .post(ClientResponse.class, object);
-  }
   
-  protected String getJson(final WebResource webResource) {
-    final String json = webResource
-            .accept(MediaType.APPLICATION_JSON)
-            .get(String.class);
-    return json;
-  }
-
-  protected <T> List<T> get(final WebResource webResource,
-                            final TypeReference<List<T>> typeReference) {
-    final String json = getJson(webResource);
-    return readJson(json, typeReference);
-  }
-
-  protected <T> T show(final String id, Class<T> clazz) {
-    return getWebResource().path(id).get(clazz);
-  }
-
-  protected <T> List<T> get(final TypeReference<List<T>> typeReference) {
-    return get(getWebResource(), typeReference);
-  }
-
-  protected <T> TypeReference<List<T>> listTypeReference(final Class<T> clazz) {
-    return new TypeReference<List<T>>() {
-    };
-  }
-
   protected <T extends HasGalaxyUrl> T setGalaxyUrl(final T bean) {
     bean.setGalaxyUrl(galaxyInstance.getGalaxyUrl());
     bean.setApiKey(galaxyInstance.getApiKey());
     return bean;
-  }
-
-  protected ClientResponse multipartPost(final WebResource resource,
-                                         final Map<String, Object> fields,
-                                         final Iterable<BodyPart> bodyParts) {
-    final WebResource.Builder builder = resource.type(MediaType.MULTIPART_FORM_DATA_TYPE);
-    final FormDataMultiPart multiPart = new FormDataMultiPart();
-    for(final Map.Entry<String, Object> fieldEntry : fields.entrySet()) {
-      final FormDataBodyPart bp = new FormDataBodyPart(fieldEntry.getKey(), fieldEntry.getValue(), MediaType.APPLICATION_JSON_TYPE);
-      multiPart.bodyPart(bp);
-    }
-
-    for(final BodyPart bodyPart : bodyParts) {
-      bodyPart.setMediaType(MediaType.APPLICATION_OCTET_STREAM_TYPE);
-      multiPart.bodyPart(bodyPart);
-    }    
-    return builder.post(ClientResponse.class, multiPart);
-  }
-  
-  protected Iterable<BodyPart> prepareUploads(final Iterable<File> files) {
-    final List<BodyPart> bodyParts = new ArrayList<BodyPart>();
-    for(final File file : files) {
-      final String paramName = "files_0|file_data";
-      final FileDataBodyPart fdbp = new FileDataBodyPart(paramName, file);
-      bodyParts.add(fdbp);
-    }
-    return bodyParts;
-  }
-
-  protected Iterable<BodyPart> prepareUpload(final File file) {
-    return prepareUploads(Arrays.asList(file));
   }
 
 }
