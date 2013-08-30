17b0ec143026810c617554e4e7a6c2fbd4553b99
John Chilton <jmchilton@gmail.com>
2013-08-29 23:39:58 -0500

    Add tool_dependency_dir to bootstrapped test instances so tool shed tools can be installed.

--- src/test/java/com/github/jmchilton/blend4j/galaxy/TestGalaxyInstance.java
+++ src/test/java/com/github/jmchilton/blend4j/galaxy/TestGalaxyInstance.java
@@ -31,6 +31,7 @@ public class TestGalaxyInstance {
       galaxyProperties.setAppProperty("allow_library_path_paste", "true");
       galaxyProperties.setAppProperty("use_remote_user", "true");
       galaxyProperties.setAppProperty("library_import_dir", ".");
+      galaxyProperties.setAppProperty("tool_dependency_dir", "tool_dependencies");
       final int port = galaxyProperties.getPort();
       
       testApiKey = adminUser.getApiKey();
