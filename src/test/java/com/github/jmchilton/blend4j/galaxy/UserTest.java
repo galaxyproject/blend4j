f92909fbda3616da09614b65810ebd86ce496b19
John Chilton <jmchilton@gmail.com>
2014-07-22 14:53:11 -0500

    Add ability to build GalaxyInstance from username and password.
    This will auto-create and fetch an API key from Galaxy.

--- src/test/java/com/github/jmchilton/blend4j/galaxy/UserTest.java
+++ src/test/java/com/github/jmchilton/blend4j/galaxy/UserTest.java
@@ -1,12 +1,15 @@
 package com.github.jmchilton.blend4j.galaxy;
 
+import com.github.jmchilton.blend4j.galaxy.beans.History;
 import com.github.jmchilton.blend4j.galaxy.beans.User;
 import com.github.jmchilton.blend4j.galaxy.beans.UserCreate;
+import com.sun.jersey.api.client.ClientResponse;
 import java.util.UUID;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class UserTest {
+  private final String TEST_PASSWORD = "testpass";
   private GalaxyInstance instance;
   private UsersClient client;
 
@@ -18,18 +21,33 @@ public class UserTest {
   
   @Test
   public void testUserCreate() {
+    final User user = createAndCheckTestUser();
+    final String apiKey = client.createApiKey(user.getId());
+  }
+  
+  @Test
+  public void testInstanceFromCredentials() {
+    final User user = createAndCheckTestUser();
+    final String url = TestGalaxyInstance.getTestInstanceUrl();
+    final GalaxyInstance authedInstance = GalaxyInstanceFactory.getFromCredentials(url, user.getEmail(), TEST_PASSWORD);
+    final History history = new History();
+    history.setName("NewUserHistory");
+    final ClientResponse response = authedInstance.getHistoriesClient().createRequest(history);
+    assert response.getStatus() == 200;
+  }
+  
+  protected User createAndCheckTestUser() {
     final UserCreate userCreate = new UserCreate();
     final String username = UUID.randomUUID().toString();
     final String email = username + "@example.com";
     userCreate.setEmail(email);
     userCreate.setUsername(username);
-    userCreate.setPassword("testpass");
+    userCreate.setPassword(TEST_PASSWORD);
     
     final User user = client.createUser(userCreate);
     assert user.getEmail().equals(email);
     assert user.getUsername().equals(username);
-    
-    final String apiKey = client.createApiKey(user.getId());
+    return user;
   }
 
 }
