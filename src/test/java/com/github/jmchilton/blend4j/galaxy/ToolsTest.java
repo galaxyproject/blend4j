b8ec28d2133e7552ac5512f68cf617c83dd41706
Franklin Bristow <fbristow@gmail.com>
2014-09-09 13:22:47 +0100

    Some basic tests for loading tool details.

--- src/test/java/com/github/jmchilton/blend4j/galaxy/ToolsTest.java
+++ src/test/java/com/github/jmchilton/blend4j/galaxy/ToolsTest.java
@@ -1,18 +1,29 @@
 package com.github.jmchilton.blend4j.galaxy;
 
-import com.github.jmchilton.blend4j.galaxy.ToolsClient.FileUploadRequest;
-import com.github.jmchilton.blend4j.galaxy.beans.History;
-import com.github.jmchilton.blend4j.galaxy.beans.OutputDataset;
-import com.github.jmchilton.blend4j.galaxy.beans.ToolExecution;
-import com.sun.jersey.api.client.ClientResponse;
 import java.io.File;
 import java.util.List;
+
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import com.github.jmchilton.blend4j.galaxy.ToolsClient.FileUploadRequest;
+import com.github.jmchilton.blend4j.galaxy.beans.OutputDataset;
+import com.github.jmchilton.blend4j.galaxy.beans.Tool;
+import com.github.jmchilton.blend4j.galaxy.beans.ToolDetails;
+import com.github.jmchilton.blend4j.galaxy.beans.ToolDetails.ToolDetailsConditionalInputs;
+import com.github.jmchilton.blend4j.galaxy.beans.ToolDetails.ToolDetailsGenomeBuildInputs;
+import com.github.jmchilton.blend4j.galaxy.beans.ToolDetails.ToolDetailsInputs;
+import com.github.jmchilton.blend4j.galaxy.beans.ToolExecution;
+import com.github.jmchilton.blend4j.galaxy.beans.ToolSection;
+import com.sun.jersey.api.client.ClientResponse;
 
 public class ToolsTest {
   private GalaxyInstance instance;
   private ToolsClient client;
+  
+  private static final String UPLOADER_TOOL_ID = "upload1";
+  
+  private static final String MEME_FIMO_TOOL_ID = "meme_fimo";
 
   @BeforeMethod
   public void init() {
@@ -41,6 +52,46 @@ public class ToolsTest {
     //assert clientResponse.getStatus() == 200;
     //final String reply = clientResponse.getEntity(String.class);
     //assert false: reply;
+  }
+  
+  /**
+   * This test is just testing the complete workflow 
+   * from loading all tools to getting an individual tool.
+   */
+  @Test
+  public void testShowToolDetails() {
+	  final List<ToolSection> tools = client.getTools();
+	  final ToolSection toolSection = tools.iterator().next();
+	  final Tool tool = toolSection.getElems().iterator().next();
+	  final ToolDetails toolDetails = client.showTool(tool.getId());
+	  
+	  // confirm that we actually received something from the server
+	  assert toolDetails != null;
+  }
+  
+  /**
+   * Test showing the details of a basic tool.
+   */
+  @Test
+  public void testShowDefaultUploadTool() {
+	  // we assume that the uploader tool comes stock with Galaxy.
+	  final ToolDetails toolDetails = client.showTool(UPLOADER_TOOL_ID);
+	  assert toolDetails != null;
+	  assert UPLOADER_TOOL_ID.equals(toolDetails.getId());
+	  // verify a couple of the inputs
+	  final List<ToolDetailsInputs> inputs = toolDetails.getInputs();
+	  assert inputs != null;
+	  assert !inputs.isEmpty();
+	  ToolDetailsGenomeBuildInputs genomeBuildInputs = null;
+	  // find the "genome" input:
+	  for (final ToolDetailsInputs input : inputs) {
+		  if (input instanceof ToolDetailsGenomeBuildInputs) {
+			  genomeBuildInputs = (ToolDetailsGenomeBuildInputs) input;
+			  break;
+		  }
+	  }
+	  
+	  assert genomeBuildInputs != null;
   }
     
   private FileUploadRequest testRequest() {
