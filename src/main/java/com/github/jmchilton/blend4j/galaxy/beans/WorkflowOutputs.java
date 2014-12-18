48ff545c1303a5b84b862afae98e6f7f49145d99
John Chilton <jmchilton@gmail.com>
2014-12-18 11:52:27 -0500

    Fix workflow running for new output parameters.

--- src/main/java/com/github/jmchilton/blend4j/galaxy/beans/WorkflowOutputs.java
+++ src/main/java/com/github/jmchilton/blend4j/galaxy/beans/WorkflowOutputs.java
@@ -2,7 +2,10 @@ package com.github.jmchilton.blend4j.galaxy.beans;
 
 import java.util.List;
 import org.codehaus.jackson.annotate.JsonProperty;
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
 
+
+@JsonIgnoreProperties(ignoreUnknown=true)
 public class WorkflowOutputs {
   private String historyId;
   private List<String> outputIds;
