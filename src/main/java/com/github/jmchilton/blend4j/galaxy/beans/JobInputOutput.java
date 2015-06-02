51e4063f4e18cf12c60edb5b8f3023de0e7dc4ad
bbimber <bbimber@gmail.com>
2015-06-02 11:38:59 -0700

    JoDetails

---
+++ src/main/java/com/github/jmchilton/blend4j/galaxy/beans/JobInputOutput.java
@@ -0,0 +1,30 @@
+package com.github.jmchilton.blend4j.galaxy.beans;
+
+
+import org.codehaus.jackson.annotate.JsonProperty;
+
+public class JobInputOutput extends GalaxyObject {
+  public static enum Source {
+    library(), hda();
+
+    private Source() {
+
+    }
+    
+    public String toJson() {
+      return name();
+    }    
+    
+  }
+
+  @JsonProperty("src")
+  private Source source;
+
+  public String getSource() {
+    return source.name();
+  }
+  
+  public void setSource(final Source source) {
+    this.source = source;
+  }
+}
