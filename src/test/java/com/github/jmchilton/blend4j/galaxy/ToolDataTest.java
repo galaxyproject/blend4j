5d2233b4fa803ff6a730abf5749028286c25198f
Dan Fornika <dfornika@gmail.com>
2017-11-01 13:08:22 -0700

    Added basic test for ToolData

---
+++ src/test/java/com/github/jmchilton/blend4j/galaxy/ToolDataTest.java
@@ -0,0 +1,29 @@
+package com.github.jmchilton.blend4j.galaxy;
+
+import com.github.jmchilton.blend4j.galaxy.beans.TabularToolDataTable;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class ToolDataTest {
+    private GalaxyInstance instance;
+    private ToolDataClient client;
+
+    @BeforeMethod
+    public void init() {
+        instance = TestGalaxyInstance.get();
+        client = instance.getToolDataClient();
+    }
+
+    @Test
+    public void testGetToolData() {
+        final List<TabularToolDataTable> toolDataTables = client.getDataTables();
+        assert toolDataTables != null;
+        assert ! toolDataTables.isEmpty();
+
+        for (final TabularToolDataTable toolDataTable : toolDataTables) {
+            assert toolDataTable.getId() != null;
+        }
+    }
+}
