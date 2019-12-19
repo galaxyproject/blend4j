cb72dbf44be93aebb5fa56442d6315ea85d7a52f
Jeffrey Thiessen <jeffreylthiessen@gmail.com>
2019-12-19 16:03:00 -0600

    Changed galaxy-bootstrap to use release branch instead of tag so release patches are applied and tests can run

--- src/test/java/com/github/jmchilton/blend4j/galaxy/TestGalaxyInstance.java
+++ src/test/java/com/github/jmchilton/blend4j/galaxy/TestGalaxyInstance.java
@@ -27,7 +27,7 @@ public class TestGalaxyInstance {
   @BeforeSuite
   public static void bootStrapGalaxy() throws URISyntaxException, IOException {
     if(getTestApiKey() == null) {
-      DownloadProperties downloadProperties = DownloadProperties.forRelease("v17.01");
+      DownloadProperties downloadProperties = DownloadProperties.forRelease("release_17.01");
       if(Boolean.getBoolean(System.getProperty("galaxy.bootstrap.github", "false"))){
         downloadProperties = DownloadProperties.wgetGithubMaster();
       }
