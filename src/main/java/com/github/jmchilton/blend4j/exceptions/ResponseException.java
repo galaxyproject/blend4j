8b365f1bc1f7a3a9028bc24f156c63d4711f7c66
Ying Feng <yingfeng-iu@users.noreply.github.com>
2021-05-07 15:27:21 -0400

    Amp 1205 userId (#9)
    * AMP-1205: rename username param to userId, to be consistent with Galaxy
    semantics
    
    * AMP-1205: populate detailedMessage in GalaxyResponseException so it
    displays in AMP log

--- src/main/java/com/github/jmchilton/blend4j/exceptions/ResponseException.java
+++ src/main/java/com/github/jmchilton/blend4j/exceptions/ResponseException.java
@@ -6,13 +6,16 @@ import com.sun.jersey.api.client.ClientResponse;
 public class ResponseException extends ApiException {
   private final Integer statusCode;
   private final String rawResponse;
-  
+    
+  /**
+   * AMP Extension
+   */  
   public ResponseException(final ClientResponse clientResponse) {
-    super();
+    super(clientResponse.getEntity(String.class));
     this.statusCode = clientResponse.getStatus();
     this.rawResponse = clientResponse.getEntity(String.class);
   }
-
+	    
   protected MoreObjects.ToStringHelper toStringHelper() {
     return MoreObjects
             .toStringHelper(getClass())
