7c186b5a539d01fcd7e47636b2c0b1777ad2d201
Rimsha Khalid <rimshakhalid992@gmail.com>
2022-09-09 12:28:59 +0500

    fixing jar issue

--- src/main/java/com/github/jmchilton/blend4j/galaxy/beans/Dataset.java
+++ src/main/java/com/github/jmchilton/blend4j/galaxy/beans/Dataset.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import org.codehaus.jackson.annotate.JsonIgnore;
 import org.codehaus.jackson.annotate.JsonIgnoreProperties;
 import org.codehaus.jackson.annotate.JsonProperty;
